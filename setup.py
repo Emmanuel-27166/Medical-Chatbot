a1967823ced36f1a85e9e74144b81dbb6e8ac625
EMMANUEL FODAY KAMARA <Emmanuel-27166>
2025-02-24 07:41:37 +0000

    Folder Structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= 'Medical Chatbot App',
+    version= '0.0.0',
+    author='Emmanuel Unuque',
+    author_email= 'efk27166@gmail.com',
+    package = find_packages(),
+    install_requires = [],
+)
